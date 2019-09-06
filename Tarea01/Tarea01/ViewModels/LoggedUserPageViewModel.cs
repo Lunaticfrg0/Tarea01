11a4bb4a7c8f9625aec48028ce1310ced041919a
Emilio Escaño <emiliojoseescano@hotmail.com>
2019-09-06 14:49:41 -0400

    Changes to AddPage

--- Tarea01/Tarea01/ViewModels/LoggedUserPageViewModel.cs
+++ Tarea01/Tarea01/ViewModels/LoggedUserPageViewModel.cs
@@ -25,7 +25,7 @@ namespace Tarea01.ViewModels
                 Contact.Add(Contacts);
             });
 
-            Contact.Add(new Contacts() { Name = "Emilio" , Number= "8096239275"}) ;
+            //Contact.Add(new Contacts() { Name = "Emilio" , Number= "8096239275"}) ;
             AddCommand = new Command(async () =>
             {
                 await App.Current.MainPage.Navigation.PushAsync(new AddContactPage());
